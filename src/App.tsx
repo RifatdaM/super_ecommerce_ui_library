cea5deaad534adf857a5143aa7f8bf6061419895
Rakibul Hasan <39544817+RifatdaM@users.noreply.github.com>
2024-01-25 17:13:08 +0600

    component list

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,31 @@ import "./App.css";
 function App() {
   return (
     <>
+      {/* list of components */}
+
+      {/* sliders 5 type */}
+      {/* category List */}
+      {/* flash sell */}
+      {/* comment section */}
+      {/* product card */}
+      {/* banners */}
+      {/* product block */}
+      {/* flash sell block */}
+      {/* promise block */}
+
+
+
+      {/* page */}
       
+      {/* home */}
+      {/* product details */}
+      {/* category */}
+      {/* flash sell */}
+      {/* cart */}
+      {/* checkout */}
+      {/* user dashboard */}
+      {/* track */}
+
     </>
   );
 }
