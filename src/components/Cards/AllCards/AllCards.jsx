b7d55a8ff044b449eec49938c81b9ecb648673cf
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-01-30 01:22:09 +0600

    Card2, Card3
    Card 2 and 3 is added

--- src/components/Cards/AllCards/AllCards.jsx
+++ src/components/Cards/AllCards/AllCards.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import Card1 from "../Card1/Card1";
+import Card2 from "../Card2/Card2";
+import Card3 from "../Card3/Card3";
 
 const AllCards = () => {
   return (
     <div>
-      <h1 className="py-10 text-4xl font-semibold">Card 1</h1>
+      {/* card 1 */}
+      <h1 className="py-10 text-5xl font-bold text-transparent bg-gradient-to-br bg-clip-text from-teal-500 via-indigo-500 to-sky-500 ">
+        Card 1
+      </h1>
       <Card1 />
+      {/* card 2 */}
+      <h1 className="py-10 text-5xl font-bold text-transparent bg-gradient-to-br bg-clip-text from-teal-500 via-indigo-500 to-sky-500 ">
+        Card 2
+      </h1>
+      <Card2 />
+      {/* card 3 */}
+      <h1 className="py-10 text-5xl font-bold text-transparent bg-gradient-to-br bg-clip-text from-teal-500 via-indigo-500 to-sky-500 ">
+        Card 3
+      </h1>
+      <Card3 />
     </div>
   );
 };
